d2810481b8d8310b58c0f521492ac50701f40683
Maddi Jitendra <116104814+Jitendra797@users.noreply.github.com>
2025-08-07 06:55:21 +0000

    Deleted unnecessary files.

--- frontend/src/app/(authenticated)/datastore/manage/table-view.tsx
+++
@@ -1,7 +0,0 @@
-import { DataTable } from '@/components/TableView/data-table';
-import { columns } from './columns';
-import type { EarthquakeData } from './page';
-
-export function TableView({ data }: { data: EarthquakeData[] }) {
-  return <DataTable columns={columns} data={data} />;
-}
