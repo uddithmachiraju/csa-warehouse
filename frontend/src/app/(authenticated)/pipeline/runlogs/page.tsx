c2d9ce56ce241ce819f10ff1ce80d568e762b1ea
Maddi Jitendra <116104814+Jitendra797@users.noreply.github.com>
2025-08-07 19:01:26 +0000

    Refactor RunLogs component layout by adjusting padding, margins, and search input styling; added a header for Technical Commands.

--- frontend/src/app/(authenticated)/pipeline/runlogs/page.tsx
+++ frontend/src/app/(authenticated)/pipeline/runlogs/page.tsx
@@ -6,16 +6,16 @@ import { ContentLayout } from '@/components/admin-panel/content-layout'
 export default function RunLogs() {
   return (
     <ContentLayout title="Run Logs">
-      <div className="h-full flex flex-col space-y-4 p-4">
-        <div className="relative p-4 shrink-0">
-          <Search className="absolute left-7 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
+      <div className="h-full flex flex-col p-6">
+      <div className="relative mb-8">
+          <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
           <Input
             type="search"
-            placeholder="Search dataset"
-            className="pl-10 w-full"
+            placeholder="Search ..."
+            className="pl-9"
           />
         </div>
-
+        <h4 className="text-lg font-semibold mb-4">Technical Commands</h4>
         <div className="space-y-4">
           <PipelineItem number={1} />
           <PipelineItem number={2} />
