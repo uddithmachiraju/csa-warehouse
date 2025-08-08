f65c5cadc8f1e9607808308d23e7cf2787931a37
uddithmachiraju <uddithmachiraju@gmail.com>
2025-08-08 03:59:34 +0000

    Implemented settings & logging, integrated ERPNext dataset puller with field-level sync, added MongoDB storage, and pipeline endpoint for dataset ingestion.

---
+++ backend/app/db/database.py
@@ -0,0 +1,16 @@
+import io 
+import os
+import requests
+import pandas as pd
+from pymongo import MongoClient
+from ..config.settings import get_database_settings 
+
+settings = get_database_settings() 
+
+# ------------------ MongoDB Setup ------------------
+MONGO_URI = str(settings.mongodb_uri)
+client = MongoClient(MONGO_URI)
+db = client["fastapi_db"]
+
+users_collection = db["users"]
+datasets_collection = db["datasets"] 
---
+++ backend/app/services/storage/mongodb_service.py
@@ -0,0 +1,44 @@
+import math 
+from datetime import datetime, timezone
+import pandas as pd
+from typing import List
+from app.db.database import datasets_collection
+
+def store_to_mongodb(dataset_id: str, dataset_df: List[dict]) -> dict:
+
+    wrapper_doc = {
+        "dataset_id": dataset_id,
+        "ingested_at": datetime.now(timezone.utc).isoformat(),
+        "record_count": len(dataset_df),
+        "data": dataset_df
+    }
+
+    result = datasets_collection.insert_one(wrapper_doc)
+
+    return {
+        "inserted_id": str(result.inserted_id),
+        "record_count": len(dataset_df),
+        "dataset_id": dataset_id,
+        "ingested_at": wrapper_doc["ingested_at"]
+    }
+
+def sanitize_document(doc):
+    def sanitize_value(value):
+        if isinstance(value, float):
+            if math.isnan(value) or math.isinf(value):
+                return None  # or use 0.0, depending on your use case
+        elif isinstance(value, dict):
+            return {k: sanitize_value(v) for k, v in value.items()}
+        elif isinstance(value, list):
+            return [sanitize_value(v) for v in value]
+        return value
+
+    doc["_id"] = str(doc["_id"])
+    return {k: sanitize_value(v) for k, v in doc.items()}
+
+def get_data_from_collection(collection = datasets_collection, limit: int = 9):
+    try:
+        documents = collection.find().limit(limit)
+        return [sanitize_document(doc) for doc in documents]
+    except Exception as e:
+        raise RuntimeError(f"Error fetching documents: {e}")
